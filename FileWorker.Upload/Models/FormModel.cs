c3cdccb0c64b89dbc76a0d979356fa04397ca7b8
sale96borca <aca801@gmail.com>
2020-08-21 20:59:42 +0200

    Fix type error

--- FileWorker.Upload/Models/FormModel.cs
+++ FileWorker.Upload/Models/FormModel.cs
@@ -4,7 +4,7 @@ namespace FileWorker.Upload.Models
 {
     public class FormModel
     {
-        public strign Name { get; set; }
+        public string Name { get; set; }
         public IFormFile File { get; set; }
     }
 }
