a207ab70cc8880479f42708d8753110042d4a5e9
sale96borca <aca801@gmail.com>
2020-08-21 20:48:36 +0200

    Remove unecesary using statements

--- FileWorker.Upload/Controllers/HomeController.cs
+++ FileWorker.Upload/Controllers/HomeController.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using FileWorker.Upload.Models;
 
 namespace FileWorker.Upload.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IWebHostEnvironment _env;
+
         public HomeController()
         {
             
