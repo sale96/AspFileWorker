dccd84a435f9aa87eb08a809014ded2d97c46593
sale96borca <aca801@gmail.com>
2020-08-21 20:29:32 +0200

    Add file diplay project

---
+++ FileWorker.Display/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FileWorker.Display.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
