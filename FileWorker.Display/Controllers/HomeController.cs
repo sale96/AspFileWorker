5a2a686c39c521ba650d8036ffa01ec186e08ea8
sale96borca <aca801@gmail.com>
2020-08-21 22:14:41 +0200

    Clean up Display project

--- FileWorker.Display/Controllers/HomeController.cs
+++ FileWorker.Display/Controllers/HomeController.cs
@@ -1,37 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using FileWorker.Display.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace FileWorker.Display.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            
         }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
