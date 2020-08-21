bd042aa488512705235a3dd05edf2c679c864c27
sale96borca <aca801@gmail.com>
2020-08-21 20:37:44 +0200

    Clean up Upload project

--- FileWorker.Upload/Startup.cs
+++ FileWorker.Upload/Startup.cs
@@ -19,14 +19,10 @@ namespace FileWorker.Upload
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +32,6 @@ namespace FileWorker.Upload
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
